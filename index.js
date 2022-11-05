650642eae83220fd2db8b7dd144677c0293f8777
Jillu Rahman Jibon <jillurahmanjibon@gmail.com>
2022-11-05 07:12:25 +0600

    secret token added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
@@ -12,9 +13,32 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jf2skzr.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
+function verifyJWT(req, res, next) {
+	const authHeader = req.headers.authorization;
+	if (!authHeader) {
+		return res.status(401).send({ message: "unAuthorized access" });
+	}
+	const token = authHeader.split(" ")[1];
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+		if (err) {
+			return res.status(403).send({ message: "unAuthorized access" });
+		}
+		req.decoded = decoded;
+		next();
+	});
+}
+
 async function run() {
 	const servicesCollection = client.db("geniusCar").collection("services");
 	const orderConnection = client.db("geniusCar").collection("orders");
+
+	app.post("/jwt", (req, res) => {
+		const user = req.body;
+		const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+			expiresIn: "20h",
+		});
+		res.send({ token });
+	});
 	app.get("/services", async (req, res) => {
 		const query = {};
 		const cursor = servicesCollection.find(query);
@@ -29,7 +53,11 @@ async function run() {
 	});
 
 	// orders api
-	app.get("/orders", async (req, res) => {
+	app.get("/orders", verifyJWT, async (req, res) => {
+		const decoded = req.decoded;
+		if (decoded.email !== req.query.email) {
+			res.status(403).send({ message: "unAuthorized access" });
+		}
 		let query = {};
 		if (req.query.email) {
 			query = {
